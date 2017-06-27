ff9fe71aa8de8366c5792d9a4182e3ceccbec0e3
SummerTech <SummerTech@10.144.4.108>
2017-06-27 13:32:40 -0400

    Worked on physics and some coins

--- mattNappo/game/game.py
+++ mattNappo/game/game.py
@@ -7,18 +7,36 @@ class Background():
     def __init__(self):
         self.img = pyglet.image.load("img/newBackground.jpg") 
         self.background = pyglet.sprite.Sprite(self.img, x=0, y=0)
-        '''
-        
-        Vf = Vi + at
-        
-        '''
-        
+class Coin():
+    def __init__(self):
+        self.value = 1
+        self.x = 0
+        self.y = 0
+        self.height = 75
+        self.width = 75
+        self.img = pyglet.image.load("img/coin.png")
+        self.spr = pyglet.sprite.Sprite(self.img, x=self.x, y=self.y)
+    def spawn(self):
+        xTemp = random.randint(20, 1000)
+        gdr = 225 + self.height
+        yTemp = random.randint(gdr, 500)
+        self.x = xTemp
+        self.y = yTemp
+        self.spr.x = self.x
+        self.spr.y = self.y
+
+coinGroup = []        
+coins = Coin()
+coinGroup.append(coins)
+coins.spawn()
+
 class Character():
     def __init__(self, xx, yy):
         self.x = xx
         self.y = yy
         self.health = 100
         self.amount = 7.5
+        self.points = 0
         self.velocity = 0
         self.up = False
         self.down = False
@@ -35,10 +53,10 @@ class Character():
             self.velocity = 0
             self.y = 255 - self.height
             self.character.y = self.y
-        if self.y >= 416 and self.y < 424: #platform one
-            if self.x >= 689 and self.x <= 901:
+        if self.y >= 383 and self.y < 394: #platform one (these numbers are the height)
+            if self.x >= 678 and self.x <= 901: # these numbers are the length
                 self.velocity = 0
-                self.y = 420
+                self.y = 393
                 self.character.y = self.y
         self.y = self.velocity + self.y
         self.character.y = self.y
@@ -50,7 +68,8 @@ class Character():
                 spr = pyglet.image.load("img/spr/right.png")
                 self.character = pyglet.sprite.Sprite(spr, x=self.x, y=self.y)
         if self.up == True:
-            self.velocity = 3.5
+            if(self.velocity == 0):
+                self.velocity = 9
             self.y = self.velocity + self.y
             self.character.y = self.y
         if self.down == True:
@@ -68,7 +87,13 @@ class Character():
             self.x = self.x + self.amount
             self.character.x = self.x
             self.lastDir = 1
-        
+    def coinDetect(self, dt):
+        if self.x == coins.x + coins.width:
+            print("hey")
+            if self.y == coins.y + coins.height:
+                print("it works")
+                self.points+=1
+                coins.spawn()
     def spiderSense(self, dt): #left and right detection
         if self.x <= 0:
             self.health-=1
@@ -78,21 +103,13 @@ class Character():
             self.health-=1
             self.x = 1357
             self.character.x = self.x
-        '''
-        if self.x >= 680 and self.x <= 900:
-            if self.y >= 320:
-                self.y = 320 + self.height
-                self.character.y = self.y
-        '''
 window = pyglet.window.Window(1440, 900)
 window.set_caption("Remake of Mario")
-
 
 background = Background()
 char = Character(45, 175)
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
-health = pyglet.text.Label(str(char.health), font_name='Times New Roman', font_size=36, x=20, y=20)
 
 @window.event
 def on_draw():
@@ -101,6 +118,9 @@ def on_draw():
     char.character.draw()
     health = pyglet.text.Label("Health: " + str(char.health), font_name='Times New Roman', font_size=36, x=20, y=20)
     health.draw()
+    points = pyglet.text.Label("Points: " + str(char.points), font_name='Times New Roman', font_size=36, x=300, y=20)
+    points.draw()
+    coins.spr.draw()
 @window.event
 def on_key_press(symbol, modifiers):
     '''if symbol == key.W:
@@ -113,6 +133,7 @@ def on_key_press(symbol, modifiers):
         char.right = True
     if symbol == key.SPACE:
         char.up = True
+
 @window.event
 def on_key_release(symbol, modifiers):
     '''if symbol == key.W:
@@ -127,4 +148,5 @@ def on_key_release(symbol, modifiers):
         char.up = False
 pyglet.clock.schedule_interval(char.move, 1/60.0)
 pyglet.clock.schedule_interval(char.spiderSense, 1/60.0)
+pyglet.clock.schedule_interval(char.coinDetect, 1/60.0)
 pyglet.app.run()
